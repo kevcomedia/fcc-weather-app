be513e12aa3c44d637eae8526dfe4cf3f122c907
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-09-01 23:05:27 +0800

    Set up babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 const gulp = require("gulp");
+const babel = require("gulp-babel");
+const rename = require("gulp-regex-rename");
 const browserSync = require("browser-sync").create();
+
+/* Compile */
+gulp.task("babel", function() {
+  return gulp.src("src/js/**/*.es6.js")
+    .pipe(babel({
+      presets: ["es2015"]
+    }))
+    .pipe(rename(/\.es6\.js$/, ".js"))
+    .pipe(gulp.dest("src/js"))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
 
 /* BrowserSync */
 gulp.task("browserSync", function() {
@@ -10,6 +25,7 @@ gulp.task("browserSync", function() {
   });
 });
 
-gulp.task("watch", ["browserSync"], function() {
+gulp.task("watch", ["babel", "browserSync"], function() {
+  gulp.watch("src/js/**/*.es6.js", ["babel"]);
   gulp.watch("src/*.html", browserSync.reload);
 });
