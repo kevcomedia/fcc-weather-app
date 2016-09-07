b1c9186d742191ae1a44e58441053fb91c5bc2df
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-09-07 16:08:46 +0800

    Write initial styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,37 @@
 const gulp = require("gulp");
-const babel = require("gulp-babel");
-const rename = require("gulp-regex-rename");
-const browserSync = require("browser-sync").create();
 
 const jshint = require("gulp-jshint");
 const stylish = require("jshint-stylish");
+
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const babel = require("gulp-babel");
+const rename = require("gulp-regex-rename");
+
+const browserSync = require("browser-sync").create();
 
 /* Lint */
 gulp.task("lint", function() {
   return gulp.src("src/js/**/*.es6.js")
     .pipe(jshint({
-        esversion: 6
+      esversion: 6
     }))
     .pipe(jshint.reporter(stylish));
 });
 
 /* Compile */
+gulp.task("sass", function() {
+  return gulp.src("src/scss/**/*.scss")
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ["> 1%"]
+    }))
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
 gulp.task("babel", ["lint"], function() {
   return gulp.src("src/js/**/*.es6.js")
     .pipe(babel({
@@ -37,7 +53,8 @@ gulp.task("browserSync", function() {
   });
 });
 
-gulp.task("watch", ["babel", "browserSync"], function() {
+gulp.task("watch", ["sass", "babel", "browserSync"], function() {
+  gulp.watch("src/scss/**/*.scss", ["sass"]);
   gulp.watch("src/js/**/*.es6.js", ["babel"]);
   gulp.watch("src/*.html", browserSync.reload);
 });
